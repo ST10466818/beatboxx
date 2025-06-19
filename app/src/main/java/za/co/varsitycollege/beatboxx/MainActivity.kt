e7967fbc5461151a0d4fb8c884e8aafc8e6e015a
https://github.com/ST10466818/packabletest.git <st10466818@vcconnect.co.za>
2025-06-19 11:51:02 +0200

    final details on first page

--- app/src/main/java/za/co/varsitycollege/beatboxx/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/beatboxx/MainActivity.kt
@@ -1,8 +1,11 @@
 package za.co.varsitycollege.beatboxx
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +18,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val startButton = findViewById<AppCompatButton>(R.id.startButton)
+        startButton.setOnClickListener {
+            Log.d("MainActivity", "Start button clicked") //Log statement for debugging
+            val intent = Intent(this, detailedDisplayView::class.java)
+            startActivity(intent)
+            }
+        val exitButton = findViewById<AppCompatButton>(R.id.exitButton)
+        exitButton.setOnClickListener {
+            finish()
+
         }
 
     }
