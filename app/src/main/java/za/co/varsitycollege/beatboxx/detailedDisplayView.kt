598e02c06b73d88a1be1cd3639feb4c8437c7ed1
https://github.com/ST10466818/packabletest.git <st10466818@vcconnect.co.za>
2025-06-19 11:02:30 +0200

    edited the xml file

--- app/src/main/java/za/co/varsitycollege/beatboxx/detailedDisplayView.kt
+++ app/src/main/java/za/co/varsitycollege/beatboxx/detailedDisplayView.kt
@@ -6,16 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class detailedDisplayView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detailed_display_view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        
     }
 }
